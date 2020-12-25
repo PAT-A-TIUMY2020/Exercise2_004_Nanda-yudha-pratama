f5573ca1410dba61c10028c7fe6cfdd586b6db30
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-12-25 14:51:34 +0700

    Add server ON/OFF

---
+++ ServerConfigRest_004_Nanda yudha pratama/Server.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Exercise2_004_Nanda_yudha_pratama;
+
+namespace ServerConfigRest_004_Nanda_yudha_pratama
+{
+    class Server
+    {
+        internal void OnServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Ready!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        internal void OffServer()
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                Console.WriteLine("Server is Off!!!");
+                Console.ReadLine();
+                hostObj.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
